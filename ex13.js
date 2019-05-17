31e3075774e72c69f3dd2937986bc624cf53c890
Viktor Zetterström <viktor.zetterstrom@appliedtechnology.se>
2019-05-17 13:36:37 +0200

    solve ex13

---
+++ ex13.js
@@ -0,0 +1,15 @@
+function repeat(operation, num) {
+  if (num <= 0) return
+
+  operation()
+
+  if (num % 5 === 0) {
+    setTimeout(() => {
+      repeat(operation, --num);
+    })
+  } else {
+    repeat(operation, --num);
+  }
+}
+
+module.exports = repeat
